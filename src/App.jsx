dc9d8d29cf47d0c01d2cd73b3df07ada2e38530f
sashin-deemantha <114796882+Sashin517@users.noreply.github.com>
2024-12-21 11:36:24 +0530

    Updated conflict in App.jsx and anding page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
-import LandingPage from '../Pages/LandingPage'
-=======
 import './App.css'
 import LandingPage from './Pages/LandingPage.jsx'
->>>>>>> 8c5d7f07cf72d3cf91d9bd9fc8544c3abf6ba02d
 
 function App() {
 
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -5,11 +5,7 @@ function LandingPage() {
   return (
     <div>
         <NaviBar />
-<<<<<<< HEAD:Pages/LandingPage.jsx
-        <h1 className="text-slate-400">Landing Page</h1>
-=======
         <h1 className='text-red-500 font-bold'>Landing Page</h1>
->>>>>>> 8c5d7f07cf72d3cf91d9bd9fc8544c3abf6ba02d:src/Pages/LandingPage.jsx
     </div>
   )
 }
