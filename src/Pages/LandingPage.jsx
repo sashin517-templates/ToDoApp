c5b970e13e8e02d48ff8b95ce08bd71f3bf906ef
Lahiru Dilshan <lahirud608@gmail.com>
2024-12-27 04:05:55 +0530

    Finalize Component

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NaviBar from '../Components/NaviBar'
+import Footer from '../Components/Footer'
 
 function LandingPage() {
   return (
     <div>
         <NaviBar />
         <h1 className='text-red-500 font-bold'>Landing Page</h1>
+        <Footer />
     </div>
   )
 }
