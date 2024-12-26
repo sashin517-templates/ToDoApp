44c094d0a04a99ef02839353675e94cc35d3a685
Lahiru Dilshan <lahirud608@gmail.com>
2024-12-26 21:33:34 +0530

    Finalize the Task View

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainLayout from "./Pages/MainLayout"; // Adjust path as necessary
-import Home from "./Pages/LandingPage";
-import Calendar from "./Pages/Calendar";
-import Profile from "./Pages/Profile";
-import AddTask from "./Pages/AddTask";
-import SingleTaskView from "./Pages/SingleTaskView";
+import SingleTaskView from "./Pages/SingleTaskView.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* MainLayout is used for all routes except the one where we hide the navbar */}
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/add-task" element={<AddTask />} />
-          <Route path="/profile" element={<Profile />} />
-        </Route>
 
-        {/* SingleTaskView does not use MainLayout, so navbar will be hidden */}
         <Route path="/SingleTaskView" element={<SingleTaskView />} />
       </Routes>
     </Router>
