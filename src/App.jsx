accf02fedea52d0dc06a4a725b539e0041bfcb5a
Lahiru Dilshan <lahirud608@gmail.com>
2024-12-27 09:28:53 +0530

    Finalized the Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import React, { useEffect } from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useTheme } from "./Components/ThemeContext.jsx";
 import LandingPage from "./Pages/LandingPage.jsx";
-import Profile from "./Pages/profile.jsx";
+import Profile from "./Pages/Profile.jsx";
 import NaviBar from "./Components/NaviBar.jsx";
+import Footer from "./Components/Footer.jsx";
 
 function App() {
-
   const { theme } = useTheme();
 
   useEffect(() => {
-    document.body.setAttribute('class', theme);
+    document.body.setAttribute("class", theme);
   }, [theme]);
 
   return (
     <Router>
-        <NaviBar />
-        <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/profile" element={<Profile />} />
-        </Routes>
+      <NaviBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     colors: {
-      transparent: 'transparent',
-      ltb: '#FFFFFF',
-      dtb: '#161221',
-      ltf: '#EDE6FF',
-      dtf: '#2D2A37',
-      primary1: '#FDBC1C',
-      primary2: '#5F33E1',
-      white: '#FFFFFF',
-      black: '#000000',
-      red: '#F52E31'
+      transparent: "transparent",
+      ltb: "#FFFFFF",
+      dtb: "#161221",
+      ltf: "#EDE6FF",
+      dtf: "#2D2A37",
+      primary1: "#FDBC1C",
+      primary2: "#5F33E1",
+      white: "#FFFFFF",
+      black: "#000000",
+      red: "#F52E31",
+      bb: "#0000001a",
     },
     extend: {},
   },
